ec31822a6ca71e33c7c93294a3ea96695a45db2e
Henning Entz <entz.henning@gmail.com>
2024-04-15 22:03:53 +0200

    Added routes for login, register, secured and wildcards to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {SecuredComponent} from "./components/secured/secured.component";
+import {authGuard} from "./core/guards/auth.guard";
 
 export const routes: Routes = [
-
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  { path: "home", redirectTo: "home", component: HomeComponent}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: "home", component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'secured', component: SecuredComponent, canActivate: [authGuard]},
+  {path: "**", component: HomeComponent}
 ];
